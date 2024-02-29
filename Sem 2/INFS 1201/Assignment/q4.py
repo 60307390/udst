8233570068b4252a8f06759887150b49f8065e07
scrumpeh-alpha <gowshikrajan690@gmail.com>
2024-02-29 19:01:37 +0300

    q4.py

---
+++ Sem 2/INFS 1201/Assignment/q4.py
@@ -0,0 +1,16 @@
+def calculateTeamPoints(matchData):
+    
+
+    return team_points
+
+
+
+
+matchData = [
+    ['Qatar', 'Iraq', 3, 2],
+    ['Jordan', 'Qatar', 1, 1],
+    ['Jordan', 'Palestine', 1, 2]
+]
+
+team_points = calculateTeamPoints(matchData)
+print(team_points)
